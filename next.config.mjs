97ba4c14553d37021b78e1794fdb54d732315ab2
keivan kazemi <kaze0017@algonquinlive.com>
2024-09-02 12:14:32 -0400

    update pwa

--- next.config.mjs
+++ next.config.mjs
@@ -20,10 +20,15 @@ import withPWA from "@ducanh2912/next-pwa";
 
 const cWithPWA = withPWA({
   dest: "public", // destination directory for the PWA files
- cacheOnFrontEndNav: true,
- aggressiveFrontEndNavCaching: true,
- reloadOnOnline: true,
-})
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 
 const nextConfig = {};
 
